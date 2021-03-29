5479b0dc507915710639603b01bc7a3d019e6e6f
Michelle Sambiase <michellesambiase@gmail.com>
2021-03-29 12:48:55 -0300

    cambios
    actualizacion de portfolio

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var intervalo;
 
+AOS.init();
+//antes media segundo parrafo 962, 965
 // A $( document ).ready() block.
-$( document ).ready(function() {
-
-  if(window.innerWidth >= 992){
+$(document).ready(function () {
+  if (window.innerWidth >= 1400) {
+    maquinaDeEscribir(152, 962);
+  }
+  if (window.innerWidth >= 1200 && window.innerWidth <= 1399) {
+    maquinaDeEscribir(152, 768);
+  }
+  if (window.innerWidth >= 992 && window.innerWidth <= 1199) {
     maquinaDeEscribir(152, 927);
-  }if(window.innerWidth <= 992 && window.innerWidth >= 768){
-    maquinaDeEscribir(152, 639)
   }
-  if(window.innerWidth <= 768 && window.innerWidth >= 500){
-    maquinaDeEscribir(152, 639)
+  if (window.innerWidth <= 992 && window.innerWidth >= 768) {
+    maquinaDeEscribir(152, 639);
   }
-  if(window.innerWidth <= 500){
-    maquinaDeEscribir(84)
+  if (window.innerWidth <= 768 && window.innerWidth >= 500) {
+    maquinaDeEscribir(152, 639);
+  }
+
+  if (window.innerWidth <= 500) {
+    maquinaDeEscribir(84);
   }
 });
-
 
 $(".skill-percent").each(function () {
   $(this).animate(
@@ -25,8 +33,8 @@ $(".skill-percent").each(function () {
     "fast"
   );
 });
-function maquinaDeEscribir(width, parrafoDosWidth) {
 
+function maquinaDeEscribir(width, parrafoDosWidth) {
   var element = document.getElementById("saludo");
   intervalo = setInterval(function () {
     //debugger;
@@ -44,7 +52,7 @@ function segundoParrafo(width) {
   elemento.classList.add("linea");
   elemento.classList.add("mostrarMeta");
   intervalo = setInterval(function () {
-   // debugger;
+    // debugger;
     if (elemento.clientWidth == width) {
       elemento.classList.remove("linea");
       tercerParrafo.classList.add("linea");
@@ -62,6 +70,7 @@ var navbar = document.getElementById("nav");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
+  //debugger;
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
@@ -69,25 +78,10 @@ function myFunction() {
   }
 }
 
-// AOS.init();
-
-var logoElement = $("footer .logo");
-
-$(window).scroll(function () {
-  if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    $(logoElement).addClass("show");
-  } else if (
-    $(logoElement).hasClass("show") &&
-    $(window).scrollTop() + $(window).height() > $(document).height() - 150
-  ) {
-    $(logoElement).removeClass("show");
-  }
-});
-
 $(document).ready(function () {
   $(this).scroll(function () {
-    var $winheight = $(window).height(); // 493 get height of window
-    var $winPos = $(document).scrollTop() + $winheight - 800; // get postion of scroll bar then add the window height and subtrack 50px, creating position 50 px above the bottom of the screen.
+    var $winheight = $(window).height(); // 651 get height of window
+    var $winPos = $(document).scrollTop() + $winheight - 600; // get postion of scroll bar then add the window height and subtrack 50px, creating position 50 px above the bottom of the screen.
     $("img").each(function () {
       // do this for each image
       var $pos = $(this).offset().top; //get the position for image
@@ -99,5 +93,31 @@ $(document).ready(function () {
         $(this).removeClass("animate");
       }
     });
+    if ($(document).scrollTop() >= 989) {
+      $(".arrows").addClass("disapear");
+    } else {
+      $(".arrows").removeClass("disapear");
+    }
   });
 });
+
+/*funcion para la card de cursos*/
+document.querySelector(".card-box").onclick = function (e) {
+  const head = e.currentTarget;
+  head.classList.toggle("active");
+};
+
+document.querySelector(".card-box-digitalers").onclick = function (e) {
+  const head = e.currentTarget;
+  head.classList.toggle("active");
+};
+
+$(document).ready(function () {
+  //   Create a function
+  $(".arrows").click(function () {
+    //       Select the body of the page and scroll down by 650 pixels worth
+    $("html, body").animate({ scrollTop: "+=989px" }, 989);
+  });
+});
+
+/*como encapsular toda la funcion para que tenga menos codigo.*/
