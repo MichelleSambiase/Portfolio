0425947c5b3a17645f427e8b3ee45082e3ca11ac
Michelle Sambiase <michellesambiase@gmail.com>
2021-04-23 20:12:52 -0300

    Cambios en HTML y Css
    Coloque una flecha interactiva que dirige hacia el inicia de la pagina

--- app.js
+++ app.js
@@ -105,3 +105,26 @@ document.querySelector(".card-box-digitalers").onclick = function (e) {
   const head = e.currentTarget;
   head.classList.toggle("active");
 };
+
+//Funcion Back to top
+
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > amountScrolled) {
+    $("button.back-to-top").addClass("show");
+  } else {
+    $("button.back-to-top").removeClass("show");
+  }
+});
+
+$("button.back-to-top").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    800
+  );
+  return false;
+});
