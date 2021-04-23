010ddd501cc4b00e2ea88b766023c747e7aeff1d
Michelle Sambiase <michellesambiase@gmail.com>
2021-04-23 20:36:48 -0300

    cambios en back to top

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ document.querySelector(".card-box-digitalers").onclick = function (e) {
 };
 
 //Funcion Back to top
-var amountScrolled = 3000;
+var amountScrolled = 3800;
 
 $(window).scroll(function () {
   if ($(window).scrollTop() > amountScrolled) {
